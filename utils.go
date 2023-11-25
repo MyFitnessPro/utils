ead0c8aea7df006e0113475549e7dccfadac1ac0
WilliamGiorno <70899240+WilliamGiorno@users.noreply.github.com>
2023-11-25 14:33:36 +0000

    Update functions to make them public

--- utils.go
+++ utils.go
@@ -15,14 +15,14 @@ func extractAndValidateQueryParams(c *gin.Context) (string, string, error) {
 	role := c.Query("role")
 
 	if uid == "" || role == "" {
-		return "", "", fmt.Errorf("Missing uid or role")
+		return "", "", fmt.Errorf("missing uid or role")
 	}
 	return uid, role, nil
 }
 
 // handleHTTPError handles the HTTP error by returning a JSON response with the error message.
 // If the error is not nil, it returns true, otherwise false.
-func handleHTTPError(c *gin.Context, err error, httpStatus int, message string) bool {
+func HandleHTTPError(c *gin.Context, err error, httpStatus int, message string) bool {
 	if err != nil {
 		c.JSON(httpStatus, gin.H{"message": fmt.Sprintf("%s: %v", message, err)})
 		c.Abort()
@@ -33,7 +33,7 @@ func handleHTTPError(c *gin.Context, err error, httpStatus int, message string) 
 
 // handleObjectError handles the object error by returning a JSON response with the error message.
 // If the object is nil, it returns true, otherwise false.
-func handleObjectError(c *gin.Context, obj interface{}, httpStatus int, message string) {
+func HandleObjectError(c *gin.Context, obj interface{}, httpStatus int, message string) {
 	if obj == nil {
 		c.JSON(httpStatus, gin.H{"message": message})
 		c.Abort()
@@ -43,16 +43,16 @@ func handleObjectError(c *gin.Context, obj interface{}, httpStatus int, message 
 // processRequest processes the HTTP request by extracting and validating query parameters,
 // binding the request body to a map, and returning the extracted data.
 // If any error occurs during the process, it returns the error.
-func processRequest(c *gin.Context, client *goFirebase.FirebaseClient) (string, string, map[string]interface{}, error) {
+func ProcessRequest(c *gin.Context, client *goFirebase.FirebaseClient) (string, string, map[string]interface{}, error) {
 	uid, role, err := extractAndValidateQueryParams(c)
-	if handleHTTPError(c, err, http.StatusBadRequest, "Invalid query parameters") {
+	if HandleHTTPError(c, err, http.StatusBadRequest, "Invalid query parameters") {
 		return "", "", nil, err
 	}
 
 	var userData map[string]interface{}
 	if c.Request.Method == http.MethodPut || c.Request.Method == http.MethodPost {
 		if err := c.BindJSON(&userData); err != nil {
-			handleHTTPError(c, err, http.StatusBadRequest, "Invalid request body")
+			HandleHTTPError(c, err, http.StatusBadRequest, "Invalid request body")
 			return "", "", nil, err
 		}
 	}
