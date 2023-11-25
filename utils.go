1b37666591de075637a44a1aeeb94d5bc4533ba6
WilliamGiorno <70899240+WilliamGiorno@users.noreply.github.com>
2023-11-25 18:15:56 +0000

    Remove unused code and update import

--- utils.go
+++ utils.go
@@ -2,23 +2,9 @@ package utils
 
 import (
 	"fmt"
-	"net/http"
 
-	goFirebase "github.com/MyFitnessPro/firebase"
 	"github.com/gin-gonic/gin"
 )
-
-// extractAndValidateQueryParams extracts and validates the query parameters.
-// If uid or role are empty, it returns an error.
-func extractAndValidateQueryParams(c *gin.Context) (string, string, error) {
-	uid := c.Query("uid")
-	role := c.Query("role")
-
-	if uid == "" || role == "" {
-		return "", "", fmt.Errorf("missing uid or role")
-	}
-	return uid, role, nil
-}
 
 // handleHTTPError handles the HTTP error by returning a JSON response with the error message.
 // If the error is not nil, it returns true, otherwise false.
@@ -38,24 +24,4 @@ func HandleObjectError(c *gin.Context, obj interface{}, httpStatus int, message 
 		c.JSON(httpStatus, gin.H{"message": message})
 		c.Abort()
 	}
-}
-
-// processRequest processes the HTTP request by extracting and validating query parameters,
-// binding the request body to a map, and returning the extracted data.
-// If any error occurs during the process, it returns the error.
-func ProcessRequest(c *gin.Context, client *goFirebase.FirebaseClient) (string, string, map[string]interface{}, error) {
-	uid, role, err := extractAndValidateQueryParams(c)
-	if HandleHTTPError(c, err, http.StatusBadRequest, "Invalid query parameters") {
-		return "", "", nil, err
-	}
-
-	var userData map[string]interface{}
-	if c.Request.Method == http.MethodPut || c.Request.Method == http.MethodPost {
-		if err := c.BindJSON(&userData); err != nil {
-			HandleHTTPError(c, err, http.StatusBadRequest, "Invalid request body")
-			return "", "", nil, err
-		}
-	}
-
-	return uid, role, userData, nil
 }
